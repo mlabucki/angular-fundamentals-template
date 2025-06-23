8ce0c6e2965691043837f0290d2538eb753f2198
Michał Łabucki <labucki.michal@gmail.com>
2025-06-23 21:20:44 +0200

    update course-card, button, pipes

--- src/app/shared/components/course-card/course-card.component.ts
+++ src/app/shared/components/course-card/course-card.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from "@angular/core";
+
+export interface Course {
+  title: string;
+  description: string;
+  creationDate: Date | string;
+  duration: number;
+  authors: string[];
+}
 
 @Component({
-  selector: 'app-course-card',
-  templateUrl: './course-card.component.html',
-  styleUrls: ['./course-card.component.scss']
+  selector: "app-course-card",
+  templateUrl: "./course-card.component.html",
+  styleUrls: ["./course-card.component.scss"],
 })
-export class CourseCardComponent {}
+export class CourseCardComponent {
+  @Input() course!: Course;
+  @Input() editable = false;
+
+  @Output() clickOnShow = new EventEmitter<Course>();
+
+  onShowClick() {
+    this.clickOnShow.emit(this.course);
+  }
+}
--- src/app/shared/pipes/duration.pipe.ts
+++ src/app/shared/pipes/duration.pipe.ts
@@ -1,8 +1,11 @@
-import { Pipe } from "@angular/core";
+import { Pipe, PipeTransform } from "@angular/core";
 
-@Pipe({
-    name: 'duration'
-})
-export class DurationPipe {
-    // Add your code here
+@Pipe({ name: "duration" })
+export class DurationPipe implements PipeTransform {
+  transform(value: number): string {
+    if (typeof value !== "number" || isNaN(value)) return "";
+    const hours = Math.floor(value / 60);
+    const minutes = value % 60;
+    return `${hours}:${minutes < 10 ? "0" : ""}${minutes} hours`;
+  }
 }
