8dbd86a857e56e34bc01e587a28a46d6758502c4
Michał Łabucki <labucki.michal@gmail.com>
2025-06-28 15:31:14 +0200

    Life cycle hooks for changes in course info component

--- src/app/features/course-info/course-info.component.ts
+++ src/app/features/course-info/course-info.component.ts
@@ -1,4 +1,12 @@
-import { Component, Input, Output, EventEmitter } from "@angular/core";
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  OnInit,
+  OnChanges,
+  SimpleChanges,
+} from "@angular/core";
 import { Course } from "../../shared/components/course-card/course-card.component";
 
 @Component({
@@ -6,10 +14,22 @@ import { Course } from "../../shared/components/course-card/course-card.componen
   templateUrl: "./course-info.component.html",
   styleUrls: ["./course-info.component.scss"],
 })
-export class CourseInfoComponent {
+export class CourseInfoComponent implements OnInit, OnChanges {
   @Input() course!: Course;
 
   @Output() backClick = new EventEmitter<void>();
+
+  ngOnInit(): void {
+    if (!this.course) {
+      console.log("Course undefined");
+    }
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes["course"]) {
+      console.log(`New value: ${changes["course"].currentValue}`);
+    }
+  }
 
   onBackClick() {
     this.backClick.emit();
