49ccf40d1bd217ba86d6a250001299f944ca0b39
Michał Łabucki <labucki.michal@gmail.com>
2025-06-28 16:38:17 +0200

    version without ngIf, fallback and pipes

--- src/app/features/course-info/course-info.component.ts
+++ src/app/features/course-info/course-info.component.ts
@@ -1,12 +1,4 @@
-import {
-  Component,
-  Input,
-  Output,
-  EventEmitter,
-  OnInit,
-  OnChanges,
-  SimpleChanges,
-} from "@angular/core";
+import { Component, Input, Output, EventEmitter } from "@angular/core";
 import { Course } from "../../shared/components/course-card/course-card.component";
 
 @Component({
@@ -14,22 +6,10 @@ import { Course } from "../../shared/components/course-card/course-card.componen
   templateUrl: "./course-info.component.html",
   styleUrls: ["./course-info.component.scss"],
 })
-export class CourseInfoComponent implements OnInit, OnChanges {
-  @Input() course?: Course;
+export class CourseInfoComponent {
+  @Input() course!: Course;
 
   @Output() backClick = new EventEmitter<void>();
-
-  ngOnInit(): void {
-    if (!this.course) {
-      console.log("Course undefined");
-    }
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes["course"]) {
-      console.log(`New value: ${changes["course"].currentValue}`);
-    }
-  }
 
   onBackClick() {
     this.backClick.emit();
