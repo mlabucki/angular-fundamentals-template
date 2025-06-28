e8606399934c004a5b1abcfa9100bbb286bbbdf3
Michał Łabucki <labucki.michal@gmail.com>
2025-06-28 14:39:00 +0200

    condition to render the course info component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectionStrategy } from "@angular/core";
+import { Component } from "@angular/core";
 import { mockedCoursesList } from "./shared/mocks/mocks";
 import { Course } from "./shared/components/course-card/course-card.component";
 
@@ -6,7 +6,6 @@ import { Course } from "./shared/components/course-card/course-card.component";
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   readonly title = "courses-app";
