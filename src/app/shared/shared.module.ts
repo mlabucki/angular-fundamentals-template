06828b1b6f9faa7dfe150ffebea714dbdd01e289
Anna Abramowicz <42539465+annaabramowicz@users.noreply.github.com>
2025-07-01 15:17:12 +0200

    Revert "deleted toggle password"
    This reverts commit 07a03cb3786e15e69e6d1a94bcfbbe79490a0c96.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -16,6 +16,7 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { DurationPipe } from "./pipes/duration.pipe";
 import { CustomDatePipe } from "./pipes/custom-date.pipe";
 import { EmailValidatorDirective } from "@shared/directives/email.directive";
+import { TogglePasswordDirective } from "@shared/directives/toggle-password.directive";
 
 const components = [
   HeaderComponent,
@@ -30,11 +31,12 @@ const components = [
   DurationPipe,
   CustomDatePipe,
   EmailValidatorDirective,
+  TogglePasswordDirective,
 ];
 
 @NgModule({
-  declarations: [components],
+  declarations: [...components],
   imports: [CommonModule, FontAwesomeModule, FormsModule, ReactiveFormsModule],
-  exports: [components],
+  exports: [...components],
 })
 export class SharedModule {}
