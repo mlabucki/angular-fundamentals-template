4a804b811c47ff7b8c0d29fd280d2fcf7e4823ab
Michał Łabucki <labucki.michal@gmail.com>
2025-07-07 23:22:01 +0200

    services and guards

--- src/app/user/services/user-store.service.ts
+++ src/app/user/services/user-store.service.ts
@@ -1,19 +1,43 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { BehaviorSubject, map } from "rxjs";
+import { UserService } from "./user.service";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: "root",
 })
 export class UserStoreService {
+  private name$$ = new BehaviorSubject<string>("");
+  private isAdmin$$ = new BehaviorSubject<boolean>(false);
 
-    getUser() {
-        // Add your code here
-    }
+  public name$ = this.name$$.asObservable();
+  public isAdmin$ = this.isAdmin$$.asObservable();
 
-    get isAdmin() {
-        // Add your code here. Get isAdmin$$ value
-    }
+  constructor(private userService: UserService) {}
 
-    set isAdmin(value: boolean) {
-        // Add your code here. Change isAdmin$$ value
-    }
+  getUser(): void {
+    // Add your code here
+    this.userService
+      .getUser()
+      .pipe(map((res) => res.result))
+      .subscribe({
+        next: (user) => {
+          this.name$$.next(user.name);
+          this.isAdmin$$.next(user.role.toLowerCase() === "admin");
+        },
+        error: () => {
+          this.name$$.next("");
+          this.isAdmin$$.next(false);
+        },
+      });
+  }
+
+  get isAdmin(): boolean {
+    // Add your code here. Get isAdmin$$ value
+    return this.isAdmin$$.value;
+  }
+
+  set isAdmin(value: boolean) {
+    // Add your code here. Change isAdmin$$ value
+    this.isAdmin$$.next(value);
+  }
 }
--- src/app/user/services/user.service.ts
+++ src/app/user/services/user.service.ts
@@ -1,10 +1,24 @@
 import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+interface UserResponse {
+  successful: boolean;
+  result: {
+    name: string;
+    role: string;
+  };
+}
 
 @Injectable({
   providedIn: "root",
 })
 export class UserService {
-  getUser() {
-    // Add your code here
+  private apiUrl = "http://localhost:4000/api";
+
+  constructor(private http: HttpClient) {}
+
+  getUser(): Observable<UserResponse> {
+    return this.http.get<UserResponse>(`${this.apiUrl}/auth/me`);
   }
 }
